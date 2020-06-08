57f0d3d96867d437a009b93d712f1293552fc4d4
Karthik Mudlapur <karthikmudlapur@Karthiks-Air.fritz.box>
2020-06-08 15:27:41 +0200

    intial commit

---
+++ __init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  8 13:39:12 2020
+
+@author: karthikmudlapur
+"""
+
---
+++ dataeda.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  8 13:39:12 2020
+
+@author: karthikmudlapur
+"""
+
+def eda(my_name):
+    """
+    Print a line about doing EDA on your data in a notebook.
+    Args:
+        my_name (str): person's name
+    Returns:
+        None
+    """
+
+    print(f"I'll do EDA on your behalf some day, {my_name}.")
